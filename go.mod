fc53102d9e2f6855a74dddb05d7f16a431ed037a
ericgogo <3892366@qq.com>
2022-01-27 14:53:55 +0800

    add main file

---
+++ go.mod
@@ -0,0 +1,5 @@
+module ericgogo/test
+
+go 1.17
+
+require github.com/ericgogo/gomodone v1.0.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "github.com/ericgogo/gomodone"
+)
+
+func main() {
+    fmt.Println(gomodone.SayHi("Roberto"))
+}
+
